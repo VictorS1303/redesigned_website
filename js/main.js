443e6e670610ead6dffb418aca6dce683ce7a15d
VictorS1303 <vict703k@stud.kea.dk>
2023-12-05 11:45:54 +0100

    Inner pages made and galleries made responsive.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+// Navbar
 const navBarToggleBtn = document.getElementById('nav_toggle_btn')
 const mainNav = document.getElementById('main_nav')
 
-
+// Image Gallery
+const galleryImageContainer = document.createElement('div')
+const galleryImages = document.querySelectorAll('.gallery-image')
 
 // EVENT LISTENERS //
 navBarToggleBtn.addEventListener('click', toggleNavBar)
@@ -15,4 +18,3 @@ function toggleNavBar()
     navBarToggleBtn.classList.toggle('active')
     mainNav.classList.toggle('active')
 }
-
